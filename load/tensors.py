conversion from position to rotation now uses tensors and can be executed on the gpu. Bug fixing, cleanup

load/tensors.py
@@ -56,7 +56,7 @@ def collate(batch):
     # Added for karate
     if 'dist' in notnone_batches[0]:
         dist = [b['dist'] for b in notnone_batches]
-        dist = torch.as_tensor(dist)
+        dist = collate_tensors(dist)
         cond['y'].update({'distance': dist})
 
     return motion, cond

preprocessing/karate/data_prep.py
@@ -7,6 +7,8 @@ import csv
 import pandas as pd
 from io import StringIO
 import numpy as np
+import torch
+
 #import data_info
 from utils.karate import data_info
 from tqdm import tqdm
@@ -49,6 +51,10 @@ def extract_meta_info(csv_file):
     # Deleting an empty row. 
     del meta_info[4]
     return meta_info
+
+def validate_csv_rows():
+    pass
+
 
 
 def extract_attacker_data(csv_file, number_of_frames, condition, participant_code, file_name):
@@ -102,7 +108,10 @@ def extract_attacker_data(csv_file, number_of_frames, condition, participant_cod
         if 'B0400-S05-E01' in file_name or 'B0400-S05-E02' in file_name:
             missing_lthi = True
             ds_labels = [l for l in ds_labels if 'LTHI' not in l]
-            df = df[ds_labels]
+            try:
+                df = df[ds_labels]
+            except Exception:
+                raise Exception('Unknown column label error.')
         else:
             raise Exception('Unknown column label error.')
 
@@ -153,15 +162,25 @@ def add_to_sample_list(sample_list, event_dfs, attacker_code, technique_cls, con
         e_df = e_df.drop('Time', axis=1, level=0)
 
         joint_positions = e_df[data_info.joint_to_index.keys()]
+        joint_positions = joint_positions.to_numpy().reshape(-1, 39, 3)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        joint_positions_tensor = torch.as_tensor(joint_positions, device=device)
         joint_axis_angles, joint_distances = geometry.calc_axis_angles_and_distances(
-            joint_positions_df=joint_positions
+            points=joint_positions_tensor
         )
-        joint_positions = joint_positions.to_numpy()
+
         joint_positions.astype(object)
 
         model_angles = e_df[model_angle_labels]
-        model_angles = model_angles.to_numpy()
+        model_angles = model_angles.to_numpy().reshape(-1, 26, 3)
         model_angles.astype(object)
+
+        joint_axis_angles = joint_axis_angles.cpu().detach().numpy()
+        joint_axis_angles.astype(object)
+
+        joint_distances = joint_distances.cpu().detach().numpy()
+        joint_distances.astype(object)
 
         sample_list.append((
             joint_positions,
@@ -178,6 +197,7 @@ def add_to_sample_list(sample_list, event_dfs, attacker_code, technique_cls, con
             experience,
             grade
         ))
+
 
 def compute_avg_LTHI_LKNEE_dist(joint_positions):
     LTHI_idx = data_info.joint_to_index['LTHI'] * 3
@@ -247,7 +267,7 @@ def main(desired_frequency, data_dir, output_dir, replace):
             message += 'replaced by new data, run this script with the --replace argument. Exiting...'
             raise Exception(message)
 
-    file_names = [dir for dir in os.listdir(data_dir) if not dir.startswith('.')]
+    file_names = [p for p in sorted(os.listdir(data_dir)) if not p.startswith('.')]
     # TODO: remove
     file_names = file_names[:10]
 
@@ -346,40 +366,59 @@ def main(desired_frequency, data_dir, output_dir, replace):
     #np.save(file_path, samples)
     #print(f'Successfully saved a numpy array with {samples.shape[0]} motion sequences at {desired_frequency} Hz.')
 
+    print(samples['joint_positions'].shape)
+    print(samples['joint_positions'][0].shape)
+    #print(samples['joint_positions'][:][:, 3:6])
+
+
     #'''
     for i in range(samples['joint_positions'].shape[0]):
         print(i)
         print('Showing exemplary motion...')
         vicon_visualization.from_array(samples['joint_positions'][i])
 
-        axis_angles = samples['joint_axis_angles'][i]
+        #exit()
+
+        axis_angles = np.expand_dims(samples['joint_axis_angles'][i], axis=0)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        axis_angles = torch.as_tensor(axis_angles, device=device)
+
         start_index = data_info.joint_to_index['T10']
-        start = samples['joint_positions'][i][:, start_index*3:start_index*3+3]
-        distances = samples['joint_distances'][i]
+        #start = samples['joint_positions'][i][:, start_index*3:start_index*3+3]
+        start = np.expand_dims(samples['joint_positions'][i][:, start_index, :], axis=0)
+        start = torch.as_tensor(start, device=device)
+
+        distances = np.expand_dims(samples['joint_distances'][i], axis=0)
+        distances = torch.as_tensor(distances, device=device)
+
         recon_pos = geometry.calc_positions(
                     chain_start_positions=start,
-                    start_label='T10', #"LFHD",
+                    start_label='T10',
                     axis_angles=axis_angles,
                     distances=distances
         )
+        recon_pos = recon_pos.cpu().detach().numpy().squeeze()
+
         print('Showing the same motion but reconstructed (should be the same)...')
         vicon_visualization.from_array(recon_pos)
     #'''
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='Preparation of the karate motion data.')
     parser.add_argument('--data_dir', '-d', dest='data_dir', type=str,
-        default='/home/anthony/pCloudDrive/storage/data/master_thesis/karate_csv/',
-        help='The directory that stores the karate csv files.')
+                        default='/home/anthony/pCloudDrive/storage/data/master_thesis/karate_csv/',
+                        help='The directory that stores the karate csv files.')
     parser.add_argument('--target_dir', '-t', dest='target_dir', type=str,
-        default='/home/anthony/pCloudDrive/storage/data/master_thesis/karate_prep/',
-        help='The directory in which the processed data will be stored in.')
+                        default='/home/anthony/pCloudDrive/storage/data/master_thesis/karate_prep/',
+                        help='The directory in which the processed data will be stored in.')
     parser.add_argument('--desired_frequency', '-f', dest='desired_frequency', type=int, default=25,
                         help='The frequency (in Hz) of the output data. \
-                            Musst be a factor of the original frequency.')
+                            Must be a factor of the original frequency.')
     parser.add_argument('--replace', '-r', dest='replace', action='store_true', default=False,
-        help='Set if the existing data in the output directory should be replaced.')
+                        help='Set if the existing data in the output directory should be replaced.')
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = get_args()

preprocessing/karate/manual_check.py
@@ -3,13 +3,8 @@
 
 import numpy as np
 import os
-import mocap_visualization
-import copy
-import data_prep
-import geometry
-import pandas as pd
-import data_info
-import json
+from visualize.vicon_visualization import from_array
+import utils.karate.data_info as data_info
 
 # Cutting: 
 # - Movement at beginning or end that is unrelated to the technique
@@ -23,10 +18,12 @@ import json
 #datapath='/home/anthony/pCloudDrive/storage/data/master_thesis/karate_prep/'
 
 # local path 
-datapath='/home/anthony/Documents/ma_data/'
+#datapath='/home/anthony/Documents/ma_data/'
+data_dir = os.path.join(os.getcwd(), 'datasets', 'KaratePoses')
 
-npydatafilepath = os.path.join(datapath, "karate_motion_25_fps_modified_outliers.npy")
-data = np.load(npydatafilepath, allow_pickle=True)
+#npydatafilepath = os.path.join(datapath, "karate_motion_25_fps_modified_outliers.npy")
+data_file_path = os.path.join(data_dir, "karate_motion_25_fps.npy")
+data = np.load(data_file_path, allow_pickle=True)
 
 # Notes: 
 # - Initial idea: Result: all defenrders are wrong oriented
@@ -53,8 +50,9 @@ data = np.load(npydatafilepath, allow_pickle=True)
 # Nevertheless, check all recording after modification.
 
 
-use_memory = True
-memory_file_path = os.path.join(datapath, 'check_idx.npy')
+use_memory = False
+memory_file_dir = os.path.join(os.getcwd(), 'preprocessing', 'karate')
+memory_file_path = os.path.join(memory_file_dir, 'check_idx.npy')
 
 if not os.path.exists(memory_file_path):
     np.save(memory_file_path, np.array([0]))
@@ -67,21 +65,19 @@ else:
 
 # TODO: add indices of problematic recordings
 # found by this manual search.
-# Also recordings with wrong technique and 
+# Also, recordings with wrong technique and
 # then ask felix if these should be cut. 
 # Argument: maybe cut only at the point where they 
-# tought the start position would be (even if wrong foot placemenet)
+# thought the start position would be (even if wrong foot placement)
 # Only remove things unrelated to technique (?)
 found_outliers = []
 
 
-
-#for idx, (i, d) in enumerate(orientation_outliers):
 for idx in range(start_point, data.shape[0]):
 
-
     print(f'Index: {idx}')
-    np.save(memory_file_path, np.array([idx]))
+    if use_memory:
+        np.save(memory_file_path, np.array([idx]))
     
     d = data['joint_positions'][idx]
     technique_cls = data['technique_cls'][idx]
@@ -89,22 +85,9 @@ for idx in range(start_point, data.shape[0]):
     condition = data['condition'][idx]
     grade = data['grade'][idx]
 
-    # 'attacker'
-    # 'air' 
-    # 'shield'
-    # 'defender'
-    #if technique == 'Ushiro-Mawashi-Geri' and condition == 'defender':
-    print(technique)
-    print(condition)
-    print(grade)
-    #mocap_visualization.from_array(d)
+    print(f'Technique: {technique}')
+    print(f'Condition: {condition}')
+    print(f'Grade: {grade}')
 
-    #d_new = d
-    #d_new[:, 1::3] *= -1
-
-    mocap_visualization.from_array(d)
+    from_array(d)
     print('------')
-
-
-
-    

utils/karate/geometry.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
 import utils.karate.data_info as data_info
-import vg
-import utils.rotation_conversions as base_geometry
 import torch
 
-def points_to_axis_angle_and_distance(start_point, end_point):
-    dist = np.linalg.norm(end_point - start_point)
+# This geometry is implemented so that is works both on the gpu and the cpu.
 
-    new_start = np.array([0., 0., 0.]) - start_point
-    new_end = end_point - start_point
-    new_start = new_start / np.linalg.norm(new_start)
-    new_end = new_end / np.linalg.norm(new_end)
 
-    axis = np.cross(new_start, new_end)
-    axis = axis / np.linalg.norm(axis)
-    signed_angle = vg.signed_angle(new_start, new_end, look=axis)
-    signed_angle = np.radians(np.array([signed_angle]))[0]
+def calc_signed_axis_angle(start, end):
+    v_cross = torch.cross(start, end, dim=-1)
+    v_dot = torch.sum(start * end, dim=-1, keepdim=True)
+    axis = torch.div(v_cross, torch.linalg.norm(v_cross, dim=-1).unsqueeze(dim=-1))
+    # The determinant of the 3x3 matrix consisting of three vectors is defined as dot((cross(start, end), axis).
+    # https://www.math.umd.edu/~petersd/241/crossprod.pdf
+    det = torch.sum(v_cross * axis, dim=-1, keepdim=True)
+    angle = torch.atan2(det, v_dot)
+    axis_angle = axis * angle
+    return axis_angle
 
-    axis_angle = axis * signed_angle
+
+def points_to_axis_angles_and_distances(start_points, end_points):
+    dist = torch.linalg.norm(end_points - start_points, dim=-1)
+    new_starts = torch.zeros_like(start_points) - start_points
+    new_ends = end_points - start_points
+
+    new_starts = torch.div(new_starts, torch.linalg.norm(new_starts, dim=-1).unsqueeze(dim=-1))
+    new_ends = torch.div(new_ends, torch.linalg.norm(new_ends, dim=-1).unsqueeze(dim=-1))
+
+    axis_angle = calc_signed_axis_angle(new_starts, new_ends)
     return axis_angle, dist
 
 
-def calc_axis_angles_and_distances(joint_positions_df):
+def calc_axis_angles_and_distances(points):
+    # This is implemented so that it works for one complete sequence.
+    # For multiple reasons this is more practical than creating a mask and processing multiple movements at once.
+    # Especially in the modification we need to perform geometry on single movements.
     skeleton = data_info.reconstruction_skeleton
-    number_of_frames = joint_positions_df.shape[0]
+    n_frames = points.shape[0]
 
-    axis_angles = np.zeros(shape=(number_of_frames, len(skeleton) * 3))
-    distances = np.zeros(shape=(len(skeleton),))
+    axis_angles = torch.zeros(size=(n_frames, len(skeleton), 3), device=points.device)
+    distances = torch.zeros(size=(len(skeleton),), device=points.device)
     for i, (s, t) in enumerate(skeleton):
-        for f in range(number_of_frames):
-            start_point = joint_positions_df[s].loc[f].to_numpy()
-            end_point = joint_positions_df[t].loc[f].to_numpy()
-            axis_angle, dist = points_to_axis_angle_and_distance(start_point, end_point)
-            axis_angles[f, i*3:i*3+3] = axis_angle
-            if f == 0:
-                distances[i] = dist
+        start_idx = data_info.joint_to_index[s]
+        end_idx = data_info.joint_to_index[t]
 
-    axis_angles.astype(object)
+        start_points = points[:, start_idx, :]
+        end_points = points[:, end_idx, :]
+        angles, dist = points_to_axis_angles_and_distances(start_points, end_points)
+
+        dist = torch.mean(dist, dim=-1)
+        distances[i] = dist
+        axis_angles[:, i, :] = angles
+
+    print(axis_angles.device, distances.device)
+
     return axis_angles, distances
 
 
@@ -54,66 +68,39 @@ def rodrigues_rotation(axes, angles, vectors):
         torch.Tensor: The batch of rotated vectors.
     """
 
-    #print('shapes')
-    #print(axes.shape)
-    #print(angles.shape)
-    #print(vectors.shape)
-
     # Make sure the batch sizes of axes, angles, and vectors match
     assert axes.shape[:-1] == angles.shape[:-1] == vectors.shape[:-1], "Batch dimensions of inputs do not match"
-
     # Broadcast angles along the last dimension of the vectors tensor
     angles = angles.expand_as(vectors)
-
     # Compute the tensor product between axes and vectors using broadcasting
     v_rot = torch.cross(axes, vectors, dim=-1)
-
     # Compute the dot product between axes and vectors
     # According to https://github.com/pytorch/pytorch/issues/18027 this is the most
     # efficient way of calculating the batch dot product.
     a_dot_v = torch.sum(axes * vectors, dim=-1, keepdim=True)
-
     # Compute the rotated vectors using Rodrigues' formula
     rotated_vectors = vectors * torch.cos(angles) + v_rot * torch.sin(angles) + axes * a_dot_v * (1 - torch.cos(angles))
-
     # Squeeze out the expanded batch dimensions and return the result
     return rotated_vectors.squeeze(-1)
 
 
 def axis_angle_and_distance_to_point(start_points, axis_angles, distances):
-    #new_start = np.array([0., 0., 0.]) - start_point
-    # Centering coordinate system
+    # Centering coordinate system at old start point and defining new start point
+    # Note that this automatically copies the device type
     new_starts = torch.zeros_like(start_points) - start_points
-    #new_starts = torch.zeros_like(start_points)   # - start_points
 
     # Normalizing
-    norms = torch.linalg.norm(new_starts, dim=2)
+    norms = torch.linalg.norm(new_starts, dim=-1)
     norms = norms.unsqueeze(dim=-1)
     new_starts = torch.div(new_starts, norms)
-    #new_start = new_start / np.linalg.norm(new_start)
 
-    #axis_angle = torch.tensor(axis_angle)
-
-    angles = torch.linalg.norm(axis_angles, dim=2)
+    angles = torch.linalg.norm(axis_angles, dim=-1)
     angles = angles.unsqueeze(dim=-1)
     axes = torch.div(axis_angles, angles)
 
-    #angles = angles.squeeze()
-
-    # TODO: check if this is correct. I used chatgpt
     rec_end_points = rodrigues_rotation(axes, angles, new_starts)
 
-    #print('scary')
-    #print(rec_end_points.shape)
-
-    #exit()
-
-    ###
-    #rot_matrix = base_geometry.axis_angle_to_matrix(axis_angles)
-    #rec_end_point = np.dot(rot_matrix, new_start)
-    distances = distances.unsqueeze(dim=-1)
-    distances = distances.unsqueeze(dim=-1)
-    #print(distances.shape)
+    distances = distances.unsqueeze(dim=-1).unsqueeze(dim=-1)
 
     rec_end_points *= distances
     rec_end_points += start_points
@@ -126,41 +113,26 @@ def calc_positions(chain_start_positions, start_label, axis_angles, distances):
     if start_label != skeleton[0][0]:
         raise Exception('Start joint and skeleton order do not match!')
 
-    print(axis_angles.shape)
-    print(chain_start_positions.shape)
-    print(distances.shape)
-    #exit()
+    n_samples, n_frames, n_joints, n_feats = axis_angles.shape
 
-    nsamples, number_of_frames, njoints, feats = axis_angles.shape
-
-    #number_of_frames = axis_angles.shape[0]
-    #joint_positions = \
-    #    np.zeros(shape=(number_of_frames, axis_angles.shape[1] + 3))
     joint_positions = \
-        torch.zeros(size=(nsamples, number_of_frames, njoints + 1, 3))
+        torch.zeros(size=(n_samples, n_frames, n_joints + 1, 3), device=axis_angles.device)
+
     start_index = data_info.joint_to_index['T10']
-    #joint_positions[:, start_index*3:start_index*3+3] = chain_start_positions
-    #print(joint_positions)
-    #print(torch.mean(joint_positions))
     joint_positions[:, :, start_index, :] = chain_start_positions
-    #print(joint_positions)
-    #print(torch.mean(joint_positions))
 
-    #exit()
-
-    #for f in range(number_of_frames):
     for i in range(len(skeleton)):
         start_label = skeleton[i][0]
         start_idx = data_info.joint_to_index[start_label]
         start_points = joint_positions[:, :, start_idx, :]
 
-        #axis_angles = axis_angles[f, i * 3:i * 3 + 3]
         ax_angles = axis_angles[:, :, i, :]
         j_distances = distances[:, i]
         end_points = axis_angle_and_distance_to_point(start_points, ax_angles, j_distances)
         end_label = skeleton[i][1]
         end_idx = data_info.joint_to_index[end_label]
-        #joint_positions[f, end_idx*3:end_idx*3+3] = end_point
         joint_positions[:, :, end_idx, :] = end_points
+
+    print(joint_positions.device)
 
     return joint_positions